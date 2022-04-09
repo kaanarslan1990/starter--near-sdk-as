3a7b6e4216857c44a3f4dddac9ff230e2ccc3ead
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-04-09 15:13:20 +0300

    Practise II-Task4

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,8 +1,23 @@
 import { storage, Context } from "near-sdk-as"
 
-// return the string 'hello world'
+// // return the string 'hello world'
+// export function helloWorld(): string {
+//   return 'hello Khan'
+// }
+
+// // return the string hello <name>
+// export function helloWorld2(name: string): string {
+//   return 'hello ' + name
+// }
+
+// // return the string hello <your_name> + hello <friend_name>
+// export function helloWorld3(names: Array<string>): string {
+//   return names.map<string>(name => 'hello ' + name).join(` `);
+// }
+// return the string 'hello <youraccount>.testnet'
 export function helloWorld(): string {
-  return 'hello world'
+  const predecessor = Context.predecessor
+  return 'hello ' + predecessor
 }
 
 // read the given key from account (contract) storage
